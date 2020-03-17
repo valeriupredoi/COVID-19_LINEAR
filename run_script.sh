ac4244d36a58f697adf002990a121311ebc0a2ac
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-03-17 17:35:27 +0000

    added runner script

---
+++ run_script.sh
@@ -0,0 +1,4 @@
+download=True
+month=3
+countries=Canada,France,Germany,Ireland,Italy,Netherlands,Romania,Spain,US,Bulgaria
+python cov_lin_models.py --countries $countries --download-data $download --month $month
