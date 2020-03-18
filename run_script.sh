d7de5f6173a599007372202838173ad031eae1db
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-03-18 01:19:11 +0000

    removed file

--- run_script.sh
+++
@@ -1,5 +0,0 @@
-download=True
-month=3
-countries=Bulgaria,Belgium,Canada,France,Germany,Ireland,Italy,Netherlands,Romania,Slovakia,Spain,US
-rm -r country_plots/**
-python cov_lin_models.py --countries $countries --download-data $download --month $month
