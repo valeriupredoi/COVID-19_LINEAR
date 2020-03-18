4182b530aa227806f9b0b1447166b9be168945d5
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-03-18 01:18:53 +0000

    local change

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,5 @@
 download=True
 month=3
 countries=Bulgaria,Belgium,Canada,France,Germany,Ireland,Italy,Netherlands,Romania,Slovakia,Spain,US
+rm -r country_plots/**
 python cov_lin_models.py --countries $countries --download-data $download --month $month
