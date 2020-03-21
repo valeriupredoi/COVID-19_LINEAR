adf670efbed7feeb8701d2776dd30d8a67985f7a
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-03-21 14:36:56 +0000

    wip refactoring

--- cov_lin_models_refactored.py
+++ cov_lin_models_refactored.py
@@ -139,74 +139,15 @@ def _compute_slowdown(x_cases, y_cases, country, month):
         plot_text_s, plot_name_s)
 
 
-def plot_countries(datasets, month, country):
-    """Plot countries data."""
-    # filter data lists
-    cases = [float(c) for c in datasets[0] if c != 'NN']
-    deaths = [float(c) for c in datasets[1] if c != 'NN']
-    deaths = [d for d in deaths if d > 0.]
-    recs = [float(c) for c in datasets[2] if c != 'NN']
+def make_evolution_plot(variable_pack, country):
+    """Make the exponential evolution plot."""
+    # unpack variables
+    (x_cases, y_cases, x_slow, y_slow, cases, deaths,
+     x_deaths, y_deaths, poly_x, poly_x_s,
+     poly_x_d, y_err, y_err_d, plot_text, plot_text_s,
+     plot_text_d, plot_name) = variable_pack
 
-    actual_days = [
-        datetime.strptime(c, "%Y-%m-%dT%H:%M:%S").day
-        for c in datasets[3] if c != 'NN'
-    ]
-    # pad for unavailable data from 1st of month
-    if actual_days[0] != 1 and actual_days[0] < 10:
-        x_cases = actual_days
-    else:
-        x_cases = [float(n) for n in range(1, len(cases) + 1)]
-
-    # logarithimc regression
-    y_cases = np.log(cases)
-    y_deaths = np.log(deaths)
-    x_deaths = [float(n) for n in range(int(x_cases[-1]) - len(deaths) + 1,
-                                        int(x_cases[-1]) + 1)]
-
-    # statistics: cases: account for slowdown
-    d_time_s = []
-    R0_s = []
-    if country in SLOWDOWN:
-        (x_cases, y_cases, x_slow, y_slow,
-        poly_x_s, R_s, y_err_s, slope_s, d_time_s, R0_s,
-        plot_text_s, plot_name_s) = _compute_slowdown(x_cases, y_cases,
-                                                      country, month)
-
-    # get linear params
-    poly_x, R, y_err, slope, d_time, R0 = get_linear_parameters(
-        x_cases,
-        y_cases)
-    if d_time_s and R0_s:
-        d_time = np.mean(np.append(d_time, d_time_s))
-        R0 = np.mean(np.append(R0, R0_s))
-
-    # plot parameters: cases
-    plot_text, plot_name = get_plot_text(slope, country,
-                                         R, d_time, R0,
-                                         x_cases,
-                                         month)
-
-    # compute average mortality
-    delta = len(cases) - len(deaths)
-    mort = np.array(deaths) / np.array(cases[delta:])
-    avg_mort = np.mean(mort)
-    stdev_mort = np.std(mort)
-
-    # statistics: deaths
-    if deaths:
-        poly_x_d, R_d, y_err_d, slope_d, d_time_d, R0_d = get_linear_parameters(
-            x_deaths,
-            y_deaths
-        )
-
-        # plot parameters: deaths
-        plot_text_d = get_deaths_plot_text(
-            slope_d, "UK",
-            R_d, d_time_d,
-            avg_mort, stdev_mort
-        )
-
-    # all in one
+    # repack some data
     y_all_real = []
     if deaths:
         y_all_real.extend(deaths)
@@ -218,12 +159,10 @@ def plot_countries(datasets, month, country):
     last_tick_real.append(y_all_real[-1])
     last_tick = np.log(last_tick_real)
 
-    # plotting cases
+    # plot
     plt.scatter(x_cases, y_cases, color='r',
                 label="Daily Cases")
     plt.plot(x_cases, poly_x, '--r')
-
-    # plot slowdown
     if country in SLOWDOWN:
         plt.axvline(SLOWDOWN[country], linewidth=2, color='orange')
         plt.scatter(x_slow, y_slow, color='g',
@@ -258,10 +197,142 @@ def plot_countries(datasets, month, country):
     plt.savefig(os.path.join("country_plots", plot_name))
     plt.close()
 
+
+def plot_countries(datasets, month, country, download):
+    """Plot countries data."""
+    # filter data lists
+    cases = [float(c) for c in datasets[0] if c != 'NN']
+    deaths = [float(c) for c in datasets[1] if c != 'NN']
+    deaths = [d for d in deaths if d > 0.]
+    recs = [float(c) for c in datasets[2] if c != 'NN']
+
+    actual_days = [
+        datetime.strptime(c, "%Y-%m-%dT%H:%M:%S").day
+        for c in datasets[3] if c != 'NN'
+    ]
+    # pad for unavailable data from 1st of month
+    if actual_days[0] != 1 and actual_days[0] < 10:
+        x_cases = actual_days
+    else:
+        x_cases = [float(n) for n in range(1, len(cases) + 1)]
+
+    # x-axis for deaths
+    x_deaths = [float(n) for n in range(int(x_cases[-1]) - len(deaths) + 1,
+                                        int(x_cases[-1]) + 1)]
+
+    # UK specific data
+    if country == "UK":
+        (x_cases, cases, x_deaths,
+        deaths, avg_mort,
+        stdev_mort) = _get_official_uk_data(download)
+
+    # log data
+    y_cases = np.log(cases)
+    y_deaths = np.log(deaths)
+
+    # statistics: cases: account for slowdown
+    d_time_s = []
+    R0_s = []
+    x_slow = y_slow = poly_x_s = R_s = y_err_s = slope_s = \
+        plot_text_s = plot_name_s = None
+
+    if country in SLOWDOWN:
+        (x_cases, y_cases, x_slow, y_slow,
+        poly_x_s, R_s, y_err_s, slope_s, d_time_s, R0_s,
+        plot_text_s, plot_name_s) = _compute_slowdown(x_cases, y_cases,
+                                                      country, month)
+
+    # get linear params
+    poly_x, R, y_err, slope, d_time, R0 = get_linear_parameters(
+        x_cases,
+        y_cases)
+    if d_time_s and R0_s:
+        d_time = np.mean(np.append(d_time, d_time_s))
+        R0 = np.mean(np.append(R0, R0_s))
+
+    # plot parameters: cases
+    plot_text, plot_name = get_plot_text(slope, country,
+                                         R, d_time, R0,
+                                         x_cases,
+                                         month)
+
+    # compute average mortality
+    if country != "UK":
+        delta = len(cases) - len(deaths)
+        mort = np.array(deaths) / np.array(cases[delta:])
+        avg_mort = np.mean(mort)
+        stdev_mort = np.std(mort)
+
+    # statistics: deaths
+    poly_x_d = R_d = y_err_d = slope_d = \
+        d_time_d = R0_d = plot_text_d = None
+    if deaths:
+        (poly_x_d, R_d, y_err_d,
+         slope_d, d_time_d, R0_d) = get_linear_parameters(
+            x_deaths,
+            y_deaths
+        )
+
+        # plot parameters: deaths
+        plot_text_d = get_deaths_plot_text(
+            slope_d, "bla",
+            R_d, d_time_d,
+            avg_mort, stdev_mort
+        )
+
+    variable_pack = (x_cases, y_cases, x_slow, y_slow,
+                     cases, deaths,
+                     x_deaths, y_deaths, poly_x, poly_x_s,
+                     poly_x_d, y_err, y_err_d, plot_text,
+                     plot_text_s, plot_text_d, plot_name)
+    make_evolution_plot(variable_pack, country)
+
     return d_time, R0
 
-def plot_official_uk_data(download):
-    """Plot the UK data starting March 1st, 2020."""
+
+def _plug_simulated_cases(y_deaths_real, slope_d):
+    """Plug smulated cases based on mortality."""
+    # simulate by death rate scenario
+    sim_y_0_real = np.array(y_deaths_real) * 200.  # mrate=0.005
+    sim_y_1_real = np.array(y_deaths_real) * 100.  # mrate=0.01
+    sim_y_2_real = np.array(y_deaths_real) * 50.  # mrate=0.02
+    sim_y_3_real = np.array(y_deaths_real) * 33.3  # mrate=0.03
+    sim_y_4_real = np.array(y_deaths_real) * 25.  # mrate=0.04
+
+    # all in one for sims
+    y_sim_real = []
+    y_sim_real.extend(y_deaths_real)
+    y_sim_real.extend(y_data_real)
+    y_sim_real.extend(sim_y_0_real)
+    y_sim_real.extend(sim_y_1_real)
+    y_sim_real.extend(sim_y_2_real)
+    y_sim_real.extend(sim_y_3_real)
+    y_sim_real.extend(sim_y_4_real)
+    y_sim = np.log(y_sim_real)
+
+    # log the simulations too
+    sim_y_0 = list(np.log(sim_y_0_real))
+    sim_y_1 = list(np.log(sim_y_1_real))
+    sim_y_2 = list(np.log(sim_y_2_real))
+    sim_y_3 = list(np.log(sim_y_3_real))
+    sim_y_4 = list(np.log(sim_y_4_real))
+
+    # forecast the cases in 10 days
+    sim_y_0_f = sim_y_0_real[-1] * np.exp(10. * slope_d)
+    sim_y_1_f = sim_y_1_real[-1] * np.exp(10. * slope_d)
+    sim_y_2_f = sim_y_2_real[-1] * np.exp(10. * slope_d)
+    sim_y_3_f = sim_y_3_real[-1] * np.exp(10. * slope_d)
+    sim_y_4_f = sim_y_4_real[-1] * np.exp(10. * slope_d)
+
+    # estimate time for 65M total infection
+    full_time_0 = (np.log(65 * 1e6) - np.log(sim_y_0_real[-1])) / slope_d
+    full_time_1 = (np.log(65 * 1e6) - np.log(sim_y_1_real[-1])) / slope_d
+    full_time_2 = (np.log(65 * 1e6) - np.log(sim_y_2_real[-1])) / slope_d
+    full_time_3 = (np.log(65 * 1e6) - np.log(sim_y_3_real[-1])) / slope_d
+
+
+def _get_official_uk_data(download):
+    """Get the official UK data starting March 1st, 2020."""
     uk_cases_url = UK_DAILY_CASES_DATA
     cases_cells = get_excel_data(uk_cases_url, "UK_cases",
                                  "DailyConfirmedCases", 2,
@@ -282,111 +353,19 @@ def plot_official_uk_data(download):
         with open("country_data/UK_deaths_history", "a") as file:
             file.write(str(death_cells[1:][0]) + "\n")
 
-    # compute avergae mortality
+    x_data = [np.float(x) for x in range(1, len(y_data_real) + 1)]
+    x_deaths = [np.float(x) for x in range(13, len(y_deaths_real) + 13)]
+
+    # compute average mortality
     mort = np.array(y_deaths_real) / np.array(y_data_real[12:])
     avg_mort = np.mean(mort)
     stdev_mort = np.std(mort)
 
-    # simulate by death rate scenario
-    sim_y_0_real = np.array(y_deaths_real) * 200.  # mrate=0.005
-    sim_y_1_real = np.array(y_deaths_real) * 100.  # mrate=0.01
-    sim_y_2_real = np.array(y_deaths_real) * 50.  # mrate=0.02
-    sim_y_3_real = np.array(y_deaths_real) * 33.3  # mrate=0.03
-    sim_y_4_real = np.array(y_deaths_real) * 25.  # mrate=0.04
+    return (x_data, y_data_real, x_deaths,
+        y_deaths_real, avg_mort, stdev_mort)
 
-    # all in one
-    y_all_real = []
-    y_all_real.extend(y_deaths_real)
-    y_all_real.extend(y_data_real)
-    y_all = np.log(y_all_real)
-    last_tick_real = []
-    last_tick_real.append(y_deaths_real[-1])
-    last_tick_real.append(y_data_real[-1])
-    last_tick = np.log(last_tick_real)
 
-    # all in one for sims
-    y_sim_real = []
-    y_sim_real.extend(y_deaths_real)
-    y_sim_real.extend(y_data_real)
-    y_sim_real.extend(sim_y_0_real)
-    y_sim_real.extend(sim_y_1_real)
-    y_sim_real.extend(sim_y_2_real)
-    y_sim_real.extend(sim_y_3_real)
-    y_sim_real.extend(sim_y_4_real)
-    y_sim = np.log(y_sim_real)
-
-    # logarithimc regression
-    y_data = np.log(y_data_real)
-    y_deaths = np.log(y_deaths_real)
-
-    # log the simulations too
-    sim_y_0 = list(np.log(sim_y_0_real))
-    sim_y_1 = list(np.log(sim_y_1_real))
-    sim_y_2 = list(np.log(sim_y_2_real))
-    sim_y_3 = list(np.log(sim_y_3_real))
-    sim_y_4 = list(np.log(sim_y_4_real))
-
-    # x-time series, days: cases start 1st March
-    # deths start 13th March
-    x_data = [np.float(x) for x in range(1, len(y_data) + 1)]
-    x_deaths = [np.float(x) for x in range(13, len(y_deaths) + 13)]
-
-    # statistics: cases
-    poly_x, R, y_err, slope, d_time, R0 = get_linear_parameters(x_data,
-                                                                y_data)
-
-    # plot parameters: cases
-    plot_text, plot_name = get_plot_text(slope, "UK-GOV",
-                                         R, d_time, R0,
-                                         x_data, month="03")
-
-    # statistics: deaths
-    poly_x_d, R_d, y_err_d, slope_d, d_time_d, R0_d = get_linear_parameters(
-        x_deaths,
-        y_deaths
-    )
-
-    # forecast the cases in 10 days
-    sim_y_0_f = sim_y_0_real[-1] * np.exp(10. * slope_d)
-    sim_y_1_f = sim_y_1_real[-1] * np.exp(10. * slope_d)
-    sim_y_2_f = sim_y_2_real[-1] * np.exp(10. * slope_d)
-    sim_y_3_f = sim_y_3_real[-1] * np.exp(10. * slope_d)
-    sim_y_4_f = sim_y_4_real[-1] * np.exp(10. * slope_d)
-
-    # estimate time for 65M total infection
-    full_time_0 = (np.log(65 * 1e6) - np.log(sim_y_0_real[-1])) / slope_d
-    full_time_1 = (np.log(65 * 1e6) - np.log(sim_y_1_real[-1])) / slope_d
-    full_time_2 = (np.log(65 * 1e6) - np.log(sim_y_2_real[-1])) / slope_d
-    full_time_3 = (np.log(65 * 1e6) - np.log(sim_y_3_real[-1])) / slope_d
-
-    # plot parameters: deaths
-    plot_text_d = get_deaths_plot_text(
-        slope_d, "UK",
-        R_d, d_time_d,
-        avg_mort, stdev_mort
-    )
-
-    # plotting cases
-    plt.scatter(x_data, y_data, color='r',
-                label="Daily Cases")
-    plt.plot(x_data, poly_x, '--r')
-    plt.scatter(x_deaths, y_deaths, marker='v',
-                color='b', label="Daily Deaths")
-    plt.plot(x_deaths, poly_x_d, '--b')
-    plt.errorbar(x_data, y_data, yerr=y_err, fmt='o', color='r')
-    plt.errorbar(x_deaths, y_deaths, yerr=y_err_d, fmt='v', color='b')
-    plt.grid()
-    plt.xlim(0., x_data[-1] + 1.5)
-    plt.ylim(0., y_data[-1] + 2.5)
-    _common_plot_stuff("UK")
-    plt.text(2., y_data[-1] + 0.5, plot_text, fontsize=8)
-    plt.text(2., y_data[-1] - 1.2, plot_text_d, fontsize=8)
-    plt.legend(loc="lower left")
-    plt.yticks(y_all, [np.int(y01) for y01 in y_all_real])
-    plt.tick_params(axis="y", labelsize=8)
-    plt.savefig(os.path.join("country_plots", plot_name))
-    plt.close()
-
+def plot_simulations():
     # plot simulated cases
     plt.scatter(x_data, y_data, color='r',
                 label="Cum. Cases")
@@ -635,9 +614,6 @@ def main():
     if args.download_data:
         download = True
 
-    # plot UK always
-    plot_official_uk_data(download)
-
     # get countries
     if not os.path.isfile(args.countries):
         countries = args.countries.split(",")
@@ -650,7 +626,8 @@ def main():
     basic_rep = []
     for country in countries:
         monthly_numbers = _get_monthly_countries_data(country, args.month)
-        d_time, R0 = plot_countries(monthly_numbers, args.month, country)
+        d_time, R0 = plot_countries(monthly_numbers,
+                                    args.month, country, download)
         double_time.append(d_time)
         basic_rep.append(R0)
 
