ad086273744086c4b639710bbc10a198aa5610de
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-03-30 22:45:41 +0100

    remmove pyc files after run

--- aggregate_run.sh
+++ aggregate_run.sh
@@ -1,4 +1,7 @@
 git status
 git pull origin master
 python cov_model/cov_lin_wrapper.py --regions US_STATES --month 3
+rm cov_model/projections/*pyc
+rm cov_model/statsanalysis/*pyc
+rm cov_model/datafinder/*pyc
 git status
