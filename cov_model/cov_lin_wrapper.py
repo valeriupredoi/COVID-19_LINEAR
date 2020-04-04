db40eda21c7ee3eb22c8ff2897e15c35b0a30ec7
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-04-04 14:04:10 +0100

    readjust the estimation for real cases

--- cov_model/cov_lin_wrapper.py
+++ cov_model/cov_lin_wrapper.py
@@ -361,8 +361,12 @@ def make_simulations_plot(variable_pack, country,
     # correction: 20 days is probably more realistic
     if slowdown_deaths is not None:
         slope_sim = slope_d_s  # / 2.0 when rates halfen roughly
+        if slope_sim > 0.2:
+            slope_sim = slope_sim / 2.0
     else:
         slope_sim = slope_d  # / 2.0 when rates halfen roughly
+        if slope_sim > 0.2:
+            slope_sim = slope_sim / 2.0
     sim_y_0_f = sim_y_0_real[-1] * np.exp(20. * slope_sim)
     sim_y_1_f = sim_y_1_real[-1] * np.exp(20. * slope_sim)
     sim_y_2_f = sim_y_2_real[-1] * np.exp(20. * slope_sim)
